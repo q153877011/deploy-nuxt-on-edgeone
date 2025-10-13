51df8115036c5ba131004c9fb7dd02ca1a53c0ee
wenyiqing <wenyiqing@tencent.com>
2025-10-13 19:55:31 +0800

    first commit;

---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  nitro: {
+    preset: 'node-server'
+  },
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true }
+})
